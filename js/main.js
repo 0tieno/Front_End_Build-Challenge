107deccacd83d08dbbb1030391e48b23e5149bdd
Otieno <ronvi035@gmail.com>
2023-12-20 18:27:56 +0300

    added scrollY navbar appear

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ function closePopup() {
     popup.classList.remove('open-popup');
 }
 
+// navbar scrollY show/hide
+// const navbar = document.querySelector('.navbar');
+// const navbarHeight = navbar.getBoundingClientRect().height;
+// document.addEventListener('scroll', () => {
+//     if (window.scrollY > navbarHeight) {
+//         navbar.classList.add('navbar--dark');
+//     } else {
+//         navbar.classList.remove('navbar--dark');
+//     }
+// });
+
+window.addEventListener('scroll', function() {
+    let navigation = document.getElementById('navbar');
+    if (window.scrollY > 200) { // Adjust the scroll threshold as needed
+        navigation.classList.add('fixed-nav');
+    } else {
+        navigation.classList.remove('fixed-nav');
+    }
+});
+
